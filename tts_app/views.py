3fd66fea049d25afd2fc96d5c8ec953edef67174
rupacesigdel <rupeshcgdl2060@gmail.com>
2024-06-11 02:04:57 +0545

    traslate one to another language in text as well audio sound

--- tts_app/views.py
+++ tts_app/views.py
@@ -2,17 +2,16 @@ from django.shortcuts import render
 from django.http import JsonResponse
 import pyttsx3
 import speech_recognition as sr
-from googletrans import Translator
-from iso639 import languages
+from googletrans import Translator, LANGUAGES
 
 def index(request):
-    languages = get_language_choices()
+    languages = [(code, name) for code, name in LANGUAGES.items()]
     return render(request, 'tts_app/index.html', {'languages': languages})
 
 def speak_text(request):
     text = request.POST.get('text', '')
     engine = pyttsx3.init()
-    engine.setProperty('rate', 150)
+    engine.setProperty('rate', 150)  # Speed control of voice
     engine.say(text)
     engine.runAndWait()
     return JsonResponse({'status': 'success'})
@@ -29,15 +28,10 @@ def recognize_speech(request):
 
 def translate_text(request):
     text = request.POST.get('text', '')
-    target_language = request.POST.get('language', 'es')
+    target_language = request.POST.get('language', 'es')  # Default to Spanish
     translator = Translator()
-    translated = translator.translate(text, dest=target_language)
-    return JsonResponse({'status': 'success', 'translated_text': translated.text})
-
-
-def get_language_choices():
-    language_choices = []
-    for lang in languages:
-        if lang.part1:
-            language_choices.append((lang.part1, lang.name))
-    return sorted(language_choices, key=lambda x: x[1])
+    try:
+        translated = translator.translate(text, dest=target_language)
+        return JsonResponse({'status': 'success', 'translated_text': translated.text})
+    except ValueError:
+        return JsonResponse({'status': 'error', 'message': 'Invalid destination language'})
